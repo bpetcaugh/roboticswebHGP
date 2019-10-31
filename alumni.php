83d0baeb6e3cd8f91349c12cf3ea4b501daf987e
woopwoopthatsthesoundofthepuneet <puneet8205@gmail.com>
2019-10-31 13:26:52 -0400

    alumni sql updates, names, title, not descriptions

--- alumni.php
+++ alumni.php
@@ -1,3 +1,20 @@
+<?php session_start(); 
+
+//Step 4
+$servername = "localhost";
+$username = "robotics";
+$password = "242forlife";
+$dbname = "robotics";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+//Step 5
+$sql = "SELECT * FROM alumni";
+$result = mysqli_query($conn, $sql);
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -113,196 +130,88 @@
       <div class="album py-5 bg-light">
           <div class="container">
             
-<!---Jack-->
-          <div class="row">
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is Jack Pinkstone. He graduated from Holy Ghost Prep in 2019. Currently he is at _______ and is finding great success while studying ______</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" data-toggle="modal" data-target="#exampleModalCenter" class="btn btn-sm btn-outline-secondary">View More</button>
-                      <!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Jack Pinkstone</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-            <img class="img-fluid" src = "./pics/thumbnail.PNG"/>
-          Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text 
-          Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text 
-          Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text  Random Latin Text 
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
+
+     
+    
+
+<?php
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+     echo "
+     <!---Jack--->
+     <div class='row'>
+       <div class='col-md-4'>
+         <div class='card mb-4 box-shadow'>
+           <img class='card-img-top' src='./pics/thumbnail.PNG' alt='Card image cap'>
+           <div class='card-body'>
+             <p class='card-text'>This is Jack Pinkstone. He graduated from Holy Ghost Prep in 2019. Currently he is at _______ and is finding great success while studying ______</p>
+             <div class='d-flex justify-content-between align-items-center'>
+               <div class='btn-group'>
+                 <button type='button' data-toggle='modal' data-target='#". $row['lastname'] . "' class='btn btn-sm btn-outline-secondary'>View More</button>
+                
+                
+                 <!-- Modal for more information -->
+<div class='modal fade' id='". $row['lastname'] . "' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+<div class='modal-dialog modal-dialog-centered' role='document'>
+ <div class='modal-content'>
+   <div class='modal-header'>
+     <h5 class='modal-title' id='exampleModalLongTitle'>" . $row['firstname'] . " ". $row['lastname'] . "</h5>
+     <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+       <span aria-hidden='true'>&times;</span>
+     </button>
+   </div>
+   <div class='modal-body'>
+       <img class='img-fluid' src = './pics/thumbnail.PNG'/>
+     ". $row['Modal Paragraph'] ." 
+   </div>
+   <div class='modal-footer'>
+     <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+   </div>
+ </div>
+</div>
+</div>
+<button type='button' data-toggle='modal' data-target='#get_in_touch_with_". $row['lastname'] . "' class='btn btn-sm btn-outline-secondary'>Get in Touch</button>
+
+<!---Modal for get in touch w/ jack-->
+<div class='modal fade' id='get_in_touch_with_". $row['lastname'] . "' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+<div class='modal-dialog modal-dialog-centered' role='document'>
+ <div class='modal-content'>
+   <div class='modal-header'>
+     <h5 class='modal-title' id='exampleModalLongTitle'>Modal title</h5>
+     <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+       <span aria-hidden='true'>&times;</span>
+     </button>
+   </div>
+   <div class='modal-body'>
+   To get in contact with ". " " . $row['firstname'] . " " . $row['lastname'] . " " . " please email our alumni team at ". " " . $row['Email'] . "
+   <div class='modal-footer'>
+     <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+   </div>
+ </div>
+</div>
+</div>
+               </div>
+               <small class='text-muted'>" . $row['firstname'] . " ". $row['lastname'] . "</small>
+             </div>
+           </div>
+         </div>
+   </div>
   </div>
-  <button type="button" data-toggle="modal" data-target="#get_in_touch_with_jack" class="btn btn-sm btn-outline-secondary">Get in Touch</button>
-
-  <!---Modal for get in touch w/ jack-->
-  <div class="modal fade" id="get_in_touch_with_jack" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-        To get in contact with Jack please email our alumni team at kjrgkjnsdfbhkvd
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
   </div>
-                    </div>
-                    <small class="text-muted">Jack Pinkstone</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!---end of jack -->
+    
 
+      
+      ";
+  }
+} else { 
+  echo "0 results";
+}
 
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top" src="./pics/thumbnail.PNG" alt="Card image cap">
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </main>
+mysqli_close($conn);
+?>  
+</main>
+    <div class="container noCarouselpadding">  
 
     <footer class="text-muted">
       <div class="container">
