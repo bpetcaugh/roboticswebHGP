f42fd73333fcae1992eaa2ca9ed4d46003b0492b
woopwoopthatsthesoundofthepuneet <puneet8205@gmail.com>
2019-11-10 17:52:25 -0500

    first featurette heading echo (picture doesnt echo yet)

--- index.php
+++ index.php
@@ -131,19 +131,34 @@
   <!-- Marketing messaging and featurettes
   ================================================== -->
   <!-- Wrap the rest of the page in another container to center all the content. -->
+<?php
+$sql = "SELECT * FROM FirstHeading"; 
+$result = mysqli_query($conn, $sql);
 
-  <div class="container marketing">
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+     echo "
+  <div class='container marketing'>
 
-      <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      <div class='row featurette'>
+          <div class='col-md-7'>
+            <h2 class='featurette-heading'>". $row['Title']."</h2>
+            <p class='lead'>". $row['Description']."</p>
           </div>
-          <div class="col-md-5">
-            <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+          <div class='col-md-5'>
+            <!-- <svg class='bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto' width='500' height='500' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid slice' focusable='false' role='img' aria-label='Placeholder: 500x500'><title>Placeholder</title><rect width='100%' height='100%' fill='#eee'/><text x='50%' y='50%' fill='#aaa' dy='.3em'>500x500</text></svg> -->
+            <img class='bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto' width='500' height='500' src='./pics/" . $row['Picture'] . "'>
           </div>
         </div>
+        ";
+      }
+    } else { 
+      echo "0 results";
+    }
     
+    
+?>
         <hr class="featurette-divider">
         <div class="row">
 <!-- Puneet Gupta made this -->
