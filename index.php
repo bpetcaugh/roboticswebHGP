39f4980f57e39d6ecc07563d73d1d62b5a195688
woopwoopthatsthesoundofthepuneet <puneet8205@gmail.com>
2019-11-11 14:29:18 -0500

    small bug fixes

--- index.php
+++ index.php
@@ -194,7 +194,6 @@ if (mysqli_num_rows($result) > 0) {
  
  $sql = "SELECT * FROM CabinetMembers";
 
-
   if (mysqli_num_rows($result2) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result2)) {
@@ -252,11 +251,11 @@ mysqli_close($conn);
     <!-- START THE FEATURETTES -->
 <?php
  $sql = "SELECT * FROM SecondHeading";
+ $result = mysqli_query($conn, $sql);
 
-
- if (mysqli_num_rows($result2) > 0) {
+ if (mysqli_num_rows($result) > 0) {
    // output data of each row
-   while($row = mysqli_fetch_assoc($result2)) {
+   while($row = mysqli_fetch_assoc($result)) {
      echo "
     <hr class='featurette-divider'>
 
